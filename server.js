f30ee1443d792782c70a0e61e3ff93ff408d9d1a
Hieu <Thanglong@gmail.com>
2022-08-01 21:39:43 +0700

    update deploy

--- server.js
+++ server.js
@@ -15,10 +15,11 @@ const forceSSL = function () {
 };
 app.use(express.static('./dist/ngx-admin'));
 
+
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname,'/dist/ngx-admin/index.html'));
 });
 
 app.use(forceSSL());
 
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
